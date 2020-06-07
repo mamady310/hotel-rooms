934c95002be87f07770d3f2586c0617c64b6cd63
Mamady Sillah <supurpisces310@msn.com>
2020-06-07 19:40:37 -0400

    created banners and rendered them on home, error, and rooms pages going to try an create services

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+
 
 export default function Home(){
     return <div>
-    <Hero />;
+    <Hero>
+        <Banner title="Lavish Rooms" subtitle="Rooms starting at $299">
+            <Link to='/rooms' className="btn-primary">
+                Rooms
+            </Link>
+        </Banner>
+    </Hero>
+    ;
     </div>
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import Hero from '../components/Hero'
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
 const Rooms = () => {
     return (
-      <Hero hero="roomsHero" />
+      <Hero hero="roomsHero">
+         <Banner title="Rooms">
+         <Link to='/' className='btn-primary'>
+                HOME
+            </Link>
+         </Banner>
+      </Hero>
     )
 };
 
