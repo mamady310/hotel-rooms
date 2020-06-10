06f343de16c77aa0d8ce80247062a6680a4d3013
Mamady Sillah <supurpisces310@msn.com>
2020-06-10 16:07:45 -0400

    created featuredroom component to display featured rooms

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { RoomContext } from ''../context';
+import { RoomContext } from "../context";
 import Loading from '../components/Loading';
-
+import Room from '../components/Room';
 export default class FeaturedRooms extends Component {
 
     static contextType = RoomContext
@@ -12,7 +12,9 @@ export default class FeaturedRooms extends Component {
         console.log(rooms);
         return (
             <div>
-               from featured rooms
+               FeaturedRooms Component
+               <Room />
+               <Loading />
             </div>
         )
     }
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import loadingPic from '../images/gif/loading-gear.gif/';
+import loadingPic from '../images/gif/loading-gear.gif';
+
 
 export default function Loading() {
     return (
         <div classname="loading">
-            <h4>loading...</h4>
+            <h4>loading rooms...</h4>
             <img src={loadingPic} alt="loading" />
         </div>
     );
