4dba6ce834fdd073e49e303450cae550b731d661
Mamady Sillah <supurpisces310@msn.com>
2020-06-11 15:03:38 -0400

    going to create a function in context.js to find each room by slug

--- README.old.md
+++ README.old.md
@@ -1,1 +1,5 @@
 # hotel-rooms
+Hotel-rooms is a website created using React.js. Local data is used to populate information. 
+
+## Installation
+Fork and clone repo. Then paste it to your local directory and run "npm i" in terminal. In the same terminal run npm start to
--- src/components/Room.js
+++ src/components/Room.js
@@ -9,7 +9,7 @@ export default function Room({room}) {
     return (
         <article className="room">
          <div className="img-container">
-           <img src={images[0] || defaultimg} alt='singleroom' />
+           <img src={images[0] || defaultimg} alt="singleroom" />
            <div className="price-top">
                 <h6>${price}</h6>
                 <p>per night</p>
