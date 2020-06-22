79f4b544647af919cd7ef8966bed5c395e51020b
Mamady Sillah <supurpisces310@msn.com>
2020-06-22 15:10:21 -0400

    attempting to deploy what I have

--- src/components/StyledHero.js
+++ src/components/StyledHero.js
@@ -1,1 +1,11 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+const violet = '##EE82EE';
+
+const StyledButton = styled.button`
+color: black;
+background: ${violet};
+font-size: 3rem;
+`;
+
+export default StyledButton;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,8 @@ import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import Services from '../components/Services';
-import FeaturedRooms from '../components/FeaturedRooms'
+import FeaturedRooms from '../components/FeaturedRooms';
+import Button from '../components/StyledHero';
 
 
 export default function Home(){
@@ -18,6 +19,7 @@ export default function Home(){
         </Hero>
         <Services />
         <FeaturedRooms />
+        <Button>Hello</Button>
     </>
-    )
+    );
 }
